ffe833b89aad6d969633139ee7a76be6cd07ea51
Jāgaro <jagaro@dayam.org>
2025-04-19 07:34:12 +0000

    More

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,39 +1,47 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
+set -euo pipefail
 
-source /etc/os-release
+[ "$(id -u)" -eq 0 ] && exit 1
 
-figlet "Release: $VERSION_CODENAME"
-figlet "CPUs: $(nproc)"
+sudo dpkg --add-architecture armhf
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get build-dep -y linux-source-6.12
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
+  build-essential \
+  crossbuild-essential-armhf \
+  debootstrap \
+  git \
+  qemu-user \
+  qemu-user-binfmt \
+  qemu-user-static \
+  binfmt-support \
+  ca-certificates \
+  device-tree-compiler \
+  u-boot-tools \
+  vboot-kernel-utils \
+  cpio \
+  gzip \
+  rsync \
+  xz-utils \
+  zip \
+  kmod \
+  figlet \
+  fakeroot \
+  dh-exec \
+  linux-source-6.12 \
+  libssl-dev \
+  libssl-dev:armhf \
+  ncurses-dev
 
 mkdir -p build
 cd build
+tar xJf /usr/src/linux-source-6.12.tar.xz
+cd linux-source-6.12
 
-if [ "$VERSION_CODENAME" == "bullseye" ]; then
-	tar xJf /usr/src/linux-source-5.10.tar.xz
-	cd linux-source-5.10
-elif [ "$VERSION_CODENAME" == "bookworm" ]; then
-	tar xJf /usr/src/linux-source-6.1.tar.xz
-	cd linux-source-6.1
-elif [ "$VERSION_CODENAME" == "trixie" ]; then
-	tar xJf /usr/src/linux-source-6.12.tar.xz
-	cd linux-source-6.12
-else
-	echo "Unsupported"
-	exit 1
-fi
+cp ../../configs/6.12 ./.config
 
-kernel_version="$(make kernelversion)"
-
-export kernel_version
-figlet "KERNEL: $kernel_version"
-# shellcheck disable=SC2206
-vers=(${kernel_version//./ })
-version="${vers[0]}.${vers[1]}"
-figlet "VERSION: $version"
-
-# Get TI firmwares
 mkdir -p firmware
 wget https://git.ti.com/cgit/processor-firmware/ti-amx3-cm3-pm-firmware/plain/bin/am335x-bone-scale-data.bin?h=ti-v4.1.y -O firmware/am335x-bone-scale-data.bin
 wget https://git.ti.com/cgit/processor-firmware/ti-amx3-cm3-pm-firmware/plain/bin/am335x-evm-scale-data.bin?h=ti-v4.1.y -O firmware/am335x-evm-scale-data.bin
@@ -41,18 +49,10 @@ wget https://git.ti.com/cgit/processor-firmware/ti-amx3-cm3-pm-firmware/plain/bi
 wget https://git.ti.com/cgit/processor-firmware/ti-amx3-cm3-pm-firmware/plain/bin/am335x-pm-firmware.elf?h=ti-v4.1.y -O firmware/am335x-pm-firmware.elf
 wget https://git.ti.com/cgit/processor-firmware/ti-amx3-cm3-pm-firmware/plain/bin/am43x-evm-scale-data.bin?h=ti-v4.1.y -O firmware/am43x-evm-scale-data.bin
 
-# Copy config, apply and build kernel
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabihf-
-cp ../../configs/"$version" ./.config
 make olddefconfig
 make bindeb-pkg "-j$(nproc)"
 make dtbs
-kver=$(make kernelrelease)
-export kver
-figlet "KVER: $kver"
-if [ "$VERSION_CODENAME" == "trixie" ]; then
-	cp ./arch/arm/boot/dts/samsung/exynos5250*.dtb ..
-else
-	cp ./arch/arm/boot/dts/exynos5250*.dtb ..
-fi
+
+cp arch/arm/boot/dts/samsung/exynos5250*.dtb ..
---
+++ build-rootfs.sh
@@ -0,0 +1,221 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+[ "$(id -u)" -eq 0 ] && exit 1
+
+CHROMEBOOK_HOSTNAME=chromebook
+
+cd build
+
+#sudo debootstrap --arch=armhf trixie rootfs http://deb.debian.org/debian/
+
+sudo tee rootfs/etc/apt/sources.list.d/debian.sources >/dev/null <<EOF
+Types: deb deb-src
+URIs: http://deb.debian.org/debian/
+Suites: trixie
+Components: main contrib non-free non-free-firmware
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+
+Types: deb deb-src
+URIs: http://deb.debian.org/debian/
+Suites: trixie-updates
+Components: main contrib non-free non-free-firmware
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+
+Types: deb deb-src
+URIs: http://deb.debian.org/debian-security/
+Suites: trixie-security/updates
+Components: main contrib non-free non-free-firmware
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+EOF
+
+sudo rm -f rootfs/etc/apt/sources.list
+
+sudo tee rootfs/etc/hosts >/dev/null <<EOF
+# /etc/hosts
+127.0.0.1       localhost $CHROMEBOOK_HOSTNAME
+
+# The following lines are desirable for IPv6 capable hosts
+::1             localhost $CHROMEBOOK_HOSTNAME ip6-localhost ip6-loopback
+ff02::1         ip6-allnodes
+ff02::2         ip6-allrouters
+EOF
+
+echo "$CHROMEBOOK_HOSTNAME" | sudo tee rootfs/etc/hostname >/dev/null
+
+sudo tee rootfs/etc/network/interfaces.d/lo >/dev/null <<EOF
+auto lo
+iface lo inet loopback
+EOF
+
+sudo tee rootfs/etc/resolv.conf >/dev/null <<EOF
+nameserver 1.1.1.1
+nameserver 1.0.0.1
+EOF
+
+sudo mkdir -p rootfs/chromebook
+
+sudo cp -f linux-*.deb rootfs/chromebook
+
+sudo tee rootfs/chromebook/setup.sh >/dev/null <<'EOF'
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+export LANG=c
+
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y
+DEBIAN_FRONTEND=noninteractive apt-get autoremove -y
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+  ca-certificates \
+  e2fsprogs \
+  firmware-libertas \
+  firmware-linux-free \
+  firmware-linux-nonfree \
+  firmware-qcom-soc \
+  firmware-realtek \
+  firmware-samsung \
+  laptop-mode-tools \
+  parted \
+  util-linux \
+  wget \
+  wpasupplicant
+
+# Install downgraded cgpt
+# https://github.com/hexdump0815/imagebuilder/blob/main/doc/important-information.md#23-09-25-cgpt-seems-to-be-broken-on-32bit-armv7l-systems-in-debian-bookworm
+(
+  cd /tmp
+  wget https://ftp.debian.org/debian/pool/main/v/vboot-utils/cgpt_0~R88-13597.B-1_armhf.deb
+  dpkg -i cgpt_0~R88-13597.B-1_armhf.deb
+  rm -f cgpt_0~R88-13597.B-1_armhf.deb
+)
+
+DEBIAN_FRONTEND=noninteractive apt-get install -y /chromebook/linux-*.deb
+apt-get clean
+
+echo "root:toor" | chpasswd
+EOF
+
+sudo mount -t proc proc rootfs/proc
+sudo mount -o bind /dev rootfs/dev
+sudo mount -o bind /dev/pts rootfs/dev/pts
+sudo chroot rootfs /usr/bin/bash /chromebook/setup.sh
+sudo umount rootfs/dev/pts
+sudo umount rootfs/dev
+sudo umount rootfs/proc
+
+sudo rm -rf rootfs/chromebook
+
+mkdir -p kernel
+cp -f rootfs/boot/vmlinuz* kernel/zImage
+mkdir -p kernel/dts
+cp -f linux-source-6.12/arch/arm/boot/dts/samsung/exynos5250*.dtb kernel/dts
+dd if=/dev/zero of=kernel/bootloader bs=512 count=1
+echo 'noinitrd console=tty0 root=PARTUUID=%U/PARTNROFF=2 rootwait rw rootfstype=ext4' >kernel/kernel-usb.config
+echo 'noinitrd console=tty0 root=/dev/mmcblk0p3 rootwait rw rootfstype=ext4' >kernel/kernel-emmc.config
+
+cat >kernel/kernel-exynos.its <<EOF
+/dts-v1/;
+
+/ {
+    description = "Chrome OS kernel image with one or more FDT blobs";
+    images {
+        kernel@1{
+            description = "kernel";
+            data = /incbin/("zImage");
+            type = "kernel_noload";
+            arch = "arm";
+            os = "linux";
+            compression = "none";
+            load = <0>;
+            entry = <0>;
+          };
+        fdt@1 {
+            description = "exynos5250-snow.dtb";
+            data = /incbin/("dts/exynos5250-snow.dtb");
+            type = "flat_dt";
+            arch = "arm";
+            compression = "none";
+            hash@1 {
+                algo = "sha1";
+            };
+        };
+        fdt@2 {
+            description = "exynos5250-snow-rev5.dtb";
+            data = /incbin/("dts/exynos5250-snow-rev5.dtb");
+            type = "flat_dt";
+            arch = "arm";
+            compression = "none";
+            hash@1 {
+                algo = "sha1";
+            };
+        };
+        fdt@3 {
+            description = "exynos5250-spring.dtb";
+            data = /incbin/("dts/exynos5250-spring.dtb");
+            type = "flat_dt";
+            arch = "arm";
+            compression = "none";
+            hash@1 {
+                algo = "sha1";
+            };
+        };
+      };
+    configurations {
+        default = "conf@1";
+        conf@1{
+            kernel = "kernel@1";
+            fdt = "fdt@1";
+          };
+        conf@2{
+            kernel = "kernel@1";
+            fdt = "fdt@2";
+          };
+        conf@3 {
+            kernel = "kernel@1";
+            fdt = "fdt@3";
+          };
+      };
+  };
+EOF
+
+( cd kernel && mkimage -D "-I dts -O dtb -p 2048" -f kernel-exynos.its kernel-exynos )
+
+mkdir -p xe303c12
+
+vbutil_kernel \
+  --arch arm \
+  --pack xe303c12/kernel_usb.bin \
+  --keyblock /usr/share/vboot/devkeys/kernel.keyblock \
+  --signprivate /usr/share/vboot/devkeys/kernel_data_key.vbprivk \
+  --version 1 \
+  --config kernel/kernel-usb.config \
+  --bootloader kernel/bootloader \
+  --vmlinuz kernel/kernel-exynos
+
+vbutil_kernel \
+  --arch arm \
+  --pack xe303c12/kernel_emmc_ext4.bin \
+  --keyblock /usr/share/vboot/devkeys/kernel.keyblock \
+  --signprivate /usr/share/vboot/devkeys/kernel_data_key.vbprivk \
+  --version 1 \
+  --config kernel/kernel-emmc.config \
+  --bootloader kernel/bootloader \
+  --vmlinuz kernel/kernel-exynos
+
+rm -f xe303c12/rootfs.tar.xz
+( cd rootfs && sudo tar pcJf ../xe303c12/rootfs.tar.xz ./* )
+sudo chown $(id -u):$(id -g) xe303c12/rootfs.tar.xz
+cp ../scripts/install.sh xe303c12
+touch xe303c12/xfce_install.sh
+
+mkdir -p ../release
+zip -r ../release/xe303c12.zip xe303c12
+
+#  abootimg cgpt fake-hwclock u-boot-tools device-tree-compiler vboot-utils vboot-kernel-utils \
+#  initramfs-tools parted sudo xz-utils wpasupplicant  \
+#  locales-all ca-certificates initramfs-tools u-boot-tools locales \
+#  console-common less network-manager git \
+#  alsa-utils pulseaudio python3 wget zstd \
